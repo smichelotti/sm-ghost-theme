1d3d0087a6da8cae10ddcd662f61bdfadd8435ee
Steve Michelotti <steve.michelotti@gmail.com>
2017-04-09 11:25:48 -0400

    major checkin after smichelotti customizations

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     cssmin = require('gulp-cssmin'),
+    zip = require('gulp-zip'),
     cmq = require('gulp-combine-media-queries');
 
 gulp.task('bundle-minify-js', function () {
@@ -13,11 +14,29 @@ gulp.task('bundle-minify-js', function () {
 });
 
 gulp.task('styles-build', function() {
-  return gulp.src('assets/css/main.css')
+  //return gulp.src('assets/css/main.css')
+  return gulp.src(['assets/css/main.css', 'assets/css/shThemeDefault.css', 'assets/css/shCore.css'])
     //.pipe(cmq())
 	.pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
+    //.pipe(concat('main_built.css'))
+    .pipe(concat('main.min.css'))
+    //.pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets/css'));
 })
 
-gulp.task('default', ['bundle-minify-js', 'styles-build']);
+//gulp.task('default', ['bundle-minify-js', 'styles-build']);
+
+gulp.task('build-all', ['bundle-minify-js', 'styles-build'], function () {
+  var source = [
+    './assets/**/*',
+    './partials/**/*',
+    '*.hbs'
+  ];
+  return gulp
+    .src(source, {base: './'})
+    .pipe(gulp.dest('dist'))
+		.pipe(zip('sm-ghost-theme.zip'))
+		.pipe(gulp.dest('./'));
+})
+
+gulp.task('default', ['build-all']);
